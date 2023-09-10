4d38dd646163ef93b6b426cfe0a9ebe5282d38da
YSY <shaoyuanyu@foxmail.com>
2023-09-10 11:51:56 +0800

    新增使用nni和tensorboardX的训练

---
+++ README.md
@@ -0,0 +1,14 @@
+# DCCRN
+implementation of "DCCRN-Deep Complex Convolution Recurrent Network for Phase-Aware Speech Enhancement" by Pytorch
+
+# Environment
+pytorch ~= 3.0.1
+numpy ~= 1.22.4
+soundfile ~= 0.12.1
+scipy == 1.7.3
+librosa ~= 0.10.1
+
+nni
+
+crc32c
+tensorboardX
---
+++ tensorboardTrain.py
@@ -0,0 +1,152 @@
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+import time
+import os
+from tensorboardX import SummaryWriter
+
+from model.dccrn_e import DCCRN
+from Datasets import PrimewordsMD2018
+from PMU.gpu_mem_track import MemTracker
+
+load_weight = False
+weight_path = "./model_weight/epoch10_weight.pt"
+
+dataset_path = "../../dataset/primewords_md_2018_set1"
+noises_path = "../../dataset/noises/dormitory_adjusted/-40db"
+
+device = "cuda:0"
+batch_size = 32
+epoch = 10
+learning_rate = 1e-2
+
+# 验证
+def validateModel(model, validate_loader):
+	model.eval()
+
+	correct = 0
+	loss = 0
+	total = 0
+
+	with torch.no_grad():
+		for x, label in validate_loader:
+			x = x.float().to(device)
+			label = label.float().to(device)
+
+			spec, pred = model(x)
+			del spec
+
+			total += 1
+			loss += model.loss(pred, label, loss_mode='SI-SNR').item()
+
+			del pred
+
+	model.train()
+
+	correct = 100 - loss/total
+
+	return correct
+
+def main():
+	# torch设置
+	torch.backends.cudnn.enabled = True
+	torch.backends.cudnn.benchmark = True
+
+	# gpu tracker
+	gpu_tracker = MemTracker()
+
+	#
+	train_dataset = PrimewordsMD2018(dataset_path="{}/train_data".format(dataset_path), noise_path=noises_path, use_rate=0.1)
+	train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+	validate_dataset = PrimewordsMD2018(dataset_path="{}/test_data".format(dataset_path), noise_path=noises_path, use_rate=0.2)
+	validate_loader = DataLoader(validate_dataset, batch_size=batch_size)
+
+	# DCCRN-E 论文中宣称的最适合用于real-time的
+	model = DCCRN()
+
+	# 加载权重
+	if load_weight:
+		print("加载权重文件... (路径: {})".format(weight_path))
+		model.load_state_dict(torch.load(weight_path))
+
+	model.to(device)
+
+	model.train()
+
+	# 损失函数
+	# model内置 SI-SNR
+
+	# 优化器 SGD
+	optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+	# 获取训练开始时间
+	train_start_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+	os.mkdir("./saved_weight/{}/".format(train_start_time))
+	tensorboard_writer = SummaryWriter('running_data/{}'.format(train_start_time))
+
+	total_step = 0
+
+	# 训练
+	for epoch_i in range(epoch):
+		print("epoch {}/{} 训练中...".format(epoch_i+1, epoch))
+
+		for step, (x, label) in enumerate(train_loader):
+			# 显示进度
+			#if step % 2 == 0:
+			#    print("#", end="")
+			total_step += 1
+			
+			# 数据指定device
+			x = x.to(device)
+			label = label.to(device)
+
+			# 数据处理(模型weight数据为torch.cuda.DoubleTensor)
+			x = x.float()
+			label = label.float()
+
+			# 预测
+			spec, pred = model(x)
+			del spec
+
+			# 计算loss
+			loss = model.loss(pred, label, loss_mode='SI-SNR')
+
+			# 梯度置零
+			optimizer.zero_grad()
+
+			# 反向传播
+			loss.backward()
+
+			# 参数更新
+			optimizer.step()
+
+			# 清理显存
+			torch.cuda.empty_cache()
+
+			# tensorboard数据可视化
+			tensorboard_writer.add_scalar("loss", loss, global_step=total_step)
+			tensorboard_writer.add_scalar("acc", 100-loss, global_step=total_step)
+
+			if step % 20 == 0:
+				print("--step {} - loss: {}".format(step+1, loss))
+		
+		print()
+
+		del x, label, pred
+		
+		if (epoch_i + 1) % 5 == 0:
+			# 保存权重
+			try:
+				torch.save(model.state_dict(), "./saved_weight/{0}/{0}_epoch{1}_weight.pt".format(train_start_time, epoch_i+1))
+			except Exception as e:
+				print(e)
+				torch.save(model.state_dict(), "./saved_weight/{0}_epoch{1}_weight.pt".format(train_start_time, epoch_i+1))
+
+			# 验证
+			print("validating...")
+			acc_train = validateModel(model, train_loader)
+			acc_val = validateModel(model, validate_loader)
+			print("第 {} 轮训练\n  训练集准确率: {}\n  验证集准确率: {}".format(epoch_i+1, acc_train, acc_val))
+
+if __name__ == '__main__':
+	main()
